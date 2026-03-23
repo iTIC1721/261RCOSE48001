99b25dbb28787de5168a2039ae139c7a0885ebbf
iTIC <32430974+iTIC1721@users.noreply.github.com>
2026-03-23 14:28:36 +0900

    [feat] 상점 씬 패널 기능 구현

--- Assets/00_Scripts/ShopManager.cs
+++ Assets/00_Scripts/ShopManager.cs
@@ -1,9 +1,49 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ShopManager : MonoBehaviour
 {
+    [Header("UI")]
+    [SerializeField] List<GameObject> panels = new List<GameObject>();
+    [SerializeField] List<GameObject> panelButtons = new List<GameObject>();
 
+    [Header("Setting")]
+    [SerializeField] Color panelButtonEnabled = Color.white;
+    [SerializeField] Color panelButtonDisabled = Color.white;
+
+    private int currentPanelIndex = 0;
+
+    private void Start()
+    {
+        ActivePanel(currentPanelIndex);
+    }
+
+    public void ActivePanel(int index)
+    {
+        if (index >= panels.Count)
+        {
+            Log.LogError($"패널 인덱스 오류: {index}");
+            return;
+        }
+
+        currentPanelIndex = index;
+
+        for (int i = 0; i < panels.Count; i++)
+        {
+            if (i == index)
+            {
+                panels[i].SetActive(true);
+                panelButtons[i].GetComponent<Image>().color = panelButtonEnabled;
+            }
+            else
+            {
+                panels[i].SetActive(false);
+                panelButtons[i].GetComponent<Image>().color = panelButtonDisabled;
+            }
+        }
+    }
 
     public void MoveToScene(string sceneName)
     {
