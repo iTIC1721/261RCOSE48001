4a3d7f5aca617793dead0f0beefa1340d87b3e3b
iTIC <paul2314@naver.com>
2026-03-25 19:45:11 +0900

    [feat] Optimizer가 double을 사용하도록 변경

--- Assets/00_Scripts/Core/FSRS/FSRSOptimizer.cs
+++ Assets/00_Scripts/Core/FSRS/FSRSOptimizer.cs
@@ -1,54 +1,52 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FSRSOptimizer
 {
-    public float lr = 0.0003f;
+    public double lr = 0.0003;
 
-    float R(float S, float t)
+    double R(double S, double t)
     {
-        return Mathf.Pow(1f + t / (9f * S), -1f);
+        return Math.Pow(1.0 + t / (9.0 * S), -1.0);
     }
 
-    float Loss(float r, float y)
+    double Loss(double r, double y)
     {
-        return -(y * Mathf.Log(r + 1e-8f) + (1 - y) * Mathf.Log(1 - r + 1e-8f));
+        return -(y * Math.Log(r + 1e-12) + (1 - y) * Math.Log(1 - r + 1e-12));
     }
 
-    float dL_dR(float r, float y)
+    double dL_dR(double r, double y)
     {
-        return (r - y) / (r * (1 - r) + 1e-8f);
+        return (r - y) / (r * (1 - r) + 1e-12);
     }
 
-    float dR_dS(float S, float t)
+    double dR_dS(double S, double t)
     {
-        float baseTerm = 1f + t / (9f * S);
-        return (t / (9f * S * S)) * Mathf.Pow(baseTerm, -2f);
+        double baseTerm = 1.0 + t / (9.0 * S);
+        return (t / (9.0 * S * S)) * Math.Pow(baseTerm, -2.0);
     }
 
-    float NextStability(float s, float d, float r, int rating, float[] w)
+    double NextStability(double s, double d, double r, int rating, double[] w)
     {
-        // rating: 1=Again, 2=Hard, 3=Good, 4=Easy
-
         if (rating == 1)
         {
-            // lapse case
-            return w[11] * Mathf.Pow(d, -w[12]) *
-                   (Mathf.Pow(s + 1f, w[13]) - 1f) *
-                   Mathf.Exp(w[14] * (1f - r));
+            return w[11] * Math.Pow(d, -w[12]) *
+                   (Math.Pow(s + 1.0, w[13]) - 1.0) *
+                   Math.Exp(w[14] * (1.0 - r));
         }
         else
         {
-            float hardPenalty = (rating == 2) ? w[15] : 1f;
-            float easyBonus = (rating == 4) ? w[16] : 1f;
+            double hardPenalty = (rating == 2) ? w[15] : 1.0;
+            double easyBonus = (rating == 4) ? w[16] : 1.0;
 
-            float growth =
-                Mathf.Exp(w[8]) *
-                (11f - d) *
-                Mathf.Pow(s, -w[9]) *
-                (Mathf.Exp((1f - r) * w[10]) - 1f);
+            double growth =
+                Math.Exp(w[8]) *
+                (11.0 - d) *
+                Math.Pow(s, -w[9]) *
+                (Math.Exp((1.0 - r) * w[10]) - 1.0);
 
-            return s * (1f + growth * hardPenalty * easyBonus);
+            return s * (1.0 + growth * hardPenalty * easyBonus);
         }
     }
 
@@ -56,97 +54,88 @@ public class FSRSOptimizer
     {
         var sequences = BuildSequences(deck);
 
+        double[] w = ConvertToDouble(deck.w);
+
         for (int e = 0; e < epochs; e++)
         {
             foreach (var seq in sequences)
             {
-                TrainSequence(deck.w, seq);
+                TrainSequence(w, seq);
             }
         }
+
+        deck.w = ConvertToFloat(w);
 
         Debug.Log("FSRS Full Optimizer Done");
     }
 
-    void TrainSequence(float[] w, List<FSRSData> seq)
+    void TrainSequence(double[] w, List<FSRSData> seq)
     {
-        float[] grad = new float[w.Length];
+        double[] grad = new double[w.Length];
 
         foreach (var data in seq)
         {
-            float d = data.d;
-            float s = data.s;
-            float t = data.t;
+            double d = data.d;
+            double s = data.s;
+            double t = data.t;
 
-            // 1. 이전 recall 확률
-            float r_prev = R(s, t);
+            double r_prev = R(s, t);
 
-            // 2. stability update
-            float s_next = NextStability(s, d, r_prev, data.rating, w);
+            double s_next = NextStability(s, d, r_prev, data.rating, w);
 
-            // 3. 다음 recall
-            float r = R(s_next, data.t_next);
+            double r = R(s_next, data.t_next);
 
-            float y = data.y;
+            double y = data.y;
 
-            // -------------------
-            // backward
-            // -------------------
-            float dl_dr = dL_dR(r, y);
-            float dr_ds = dR_dS(s_next, data.t_next);
+            double dl_dr = dL_dR(r, y);
+            double dr_ds = dR_dS(s_next, data.t_next);
 
-            float dL_dS = dl_dr * dr_ds;
+            double dL_dS = dl_dr * dr_ds;
 
             for (int i = 0; i < w.Length; i++)
             {
-                float dS_dw = dNextStability_dw(i, s, d, r_prev, data.rating, w);
-
+                double dS_dw = dNextStability_dw(i, s, d, r_prev, data.rating, w);
                 grad[i] += dL_dS * dS_dw;
             }
         }
 
-        // -------------------
-        // update
-        // -------------------
-        //string tmp = "Grad:\n";
         for (int i = 0; i < w.Length; i++)
         {
-            //tmp += $"Learning[{i}]: " + (lr * grad[i]).ToString() + "\n";
             w[i] -= lr * grad[i];
         }
-        //Log.LogMessage(tmp);
     }
 
-    float dNextStability_dw(int i, float s, float d, float r, int rating, float[] w)
+    double dNextStability_dw(int i, double s, double d, double r, int rating, double[] w)
     {
         if (rating == 1)
         {
-            float baseVal =
-                Mathf.Pow(d, -w[12]) *
-                (Mathf.Pow(s + 1f, w[13]) - 1f) *
-                Mathf.Exp(w[14] * (1f - r));
+            double baseVal =
+                Math.Pow(d, -w[12]) *
+                (Math.Pow(s + 1.0, w[13]) - 1.0) *
+                Math.Exp(w[14] * (1.0 - r));
 
             if (i == 11) return baseVal;
-            if (i == 12) return -w[11] * baseVal * Mathf.Log(d);
-            if (i == 13) return w[11] * baseVal * Mathf.Log(s + 1f);
-            if (i == 14) return w[11] * baseVal * (1f - r);
+            if (i == 12) return -w[11] * baseVal * Math.Log(d);
+            if (i == 13) return w[11] * baseVal * Math.Log(s + 1.0);
+            if (i == 14) return w[11] * baseVal * (1.0 - r);
         }
         else
         {
-            float expTerm = Mathf.Exp(w[8]);
-            float growth =
+            double expTerm = Math.Exp(w[8]);
+            double growth =
                 expTerm *
-                (11f - d) *
-                Mathf.Pow(s, -w[9]) *
-                (Mathf.Exp((1f - r) * w[10]) - 1f);
+                (11.0 - d) *
+                Math.Pow(s, -w[9]) *
+                (Math.Exp((1.0 - r) * w[10]) - 1.0);
 
             if (i == 8) return s * growth;
-            if (i == 9) return -s * growth * Mathf.Log(s);
-            if (i == 10) return s * growth * (1f - r);
+            if (i == 9) return -s * growth * Math.Log(s);
+            if (i == 10) return s * growth * (1.0 - r);
             if (i == 15 && rating == 2) return s * growth;
             if (i == 16 && rating == 4) return s * growth;
         }
 
-        return 0f;
+        return 0.0;
     }
 
     List<List<FSRSData>> BuildSequences(Deck deck)
@@ -168,5 +157,21 @@ public class FSRSOptimizer
         }
 
         return sequences;
+    }
+
+    double[] ConvertToDouble(float[] arr)
+    {
+        double[] result = new double[arr.Length];
+        for (int i = 0; i < arr.Length; i++)
+            result[i] = arr[i];
+        return result;
+    }
+
+    float[] ConvertToFloat(double[] arr)
+    {
+        float[] result = new float[arr.Length];
+        for (int i = 0; i < arr.Length; i++)
+            result[i] = (float)arr[i];
+        return result;
     }
 }
