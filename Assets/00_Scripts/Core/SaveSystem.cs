1eee23c96e0552ac6d248cf0bc490f0723b841cb
iTIC <32430974+iTIC1721@users.noreply.github.com>
2026-03-30 14:27:28 +0900

    [feat] 플레이어 세이브파일 암호화 구현

--- Assets/00_Scripts/Core/SaveSystem.cs
+++ Assets/00_Scripts/Core/SaveSystem.cs
@@ -1,4 +1,7 @@
+using System;
 using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 using UnityEngine;
 
 public enum SaveType
@@ -57,7 +60,8 @@ public static class SaveSystem
     public static void SavePlayerData(PlayerSaveData data)
     {
         string json = JsonUtility.ToJson(data, true);
-        File.WriteAllText(GetPlayerDataPath(), json);
+        string encryptedJson = AESHelper.Encrypt(json, AESHelper.commonKey);
+        File.WriteAllText(GetPlayerDataPath(), encryptedJson);
 
         Log.LogMessage($"Saved Inventory: {GetPlayerDataPath()}");
     }
@@ -73,8 +77,42 @@ public static class SaveSystem
             return data;
         }
 
-        string json = File.ReadAllText(path, System.Text.Encoding.UTF8);
+        string encryptedJson = File.ReadAllText(path, System.Text.Encoding.UTF8);
+        string json = AESHelper.Decrypt(encryptedJson, AESHelper.commonKey);
         return JsonUtility.FromJson<PlayerSaveData>(json);
     }
     #endregion
+}
+
+public static class AESHelper
+{
+    public const string commonKey = "MemorixMemorix00";
+
+    public static string Encrypt(string text, string key)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.IV = new byte[16]; // IV는 0으로 초기화하여 사용
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+            byte[] textBytes = Encoding.UTF8.GetBytes(text);
+
+            byte[] encryptedBytes = encryptor.TransformFinalBlock(textBytes, 0, textBytes.Length);
+            return Convert.ToBase64String(encryptedBytes);
+        }
+    }
+
+    public static string Decrypt(string encryptedText, string key)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.IV = new byte[16]; // IV는 암호화할 때와 동일하게 설정
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+            byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
+
+            byte[] decryptedBytes = decryptor.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+            return Encoding.UTF8.GetString(decryptedBytes);
+        }
+    }
 }
