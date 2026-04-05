1a9f3fb9f720253976e93ef99fab5d9a9e7e2b51
iTIC <paul2314@naver.com>
2026-04-05 19:56:30 +0900

    [feat] HP 바 체력 감소 이펙트 추가

--- Assets/00_Scripts/HPBar.cs
+++ Assets/00_Scripts/HPBar.cs
@@ -1,13 +1,20 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HPBar : MonoBehaviour
 {
-    public Image fill;
     public TextMeshProUGUI hpText;
+    public Image fill;
+    public Image backFill;
+
+    public float backFillSpeed = 0.5f;
 
     private Entity parent;
+
+    private Coroutine backFillCoroutine;
+    private float backFillTargetAmount = 0;
 
     private void Awake()
     {
@@ -23,7 +30,31 @@ public class HPBar : MonoBehaviour
 
     public void SetHPBar(float currentHP, float maxHP)
     {
-        fill.fillAmount = currentHP / maxHP;
         hpText.text = currentHP.ToString();
+
+        float amount = currentHP / maxHP;
+        fill.fillAmount = amount;
+
+        if (backFill != null)
+        {
+            backFillTargetAmount = amount;
+            if (backFillCoroutine == null) backFillCoroutine = StartCoroutine(BackFillCoroutine());
+        }
+    }
+
+    private IEnumerator BackFillCoroutine()
+    {
+        float amount = backFill.fillAmount;
+
+        while (amount >= backFillTargetAmount)
+        {
+            yield return null;
+
+            amount -= backFillSpeed * Time.deltaTime;
+            backFill.fillAmount = amount;
+        }
+
+        backFill.fillAmount = backFillTargetAmount;
+        backFillCoroutine = null;
     }
 }
