4b88ad24c775991dbe1006645b4d4612429df993
iTIC <paul2314@naver.com>
2026-04-14 19:16:55 +0900

    [feat] AddDamage ScriptableObject 생성

---
+++ Assets/00_Scripts/ScriptableObjects/SkillEffect/AddDamageSkillEffect.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AddDamage", menuName = "Skill Effect/AddDamage")]
+public class AddDamageSkillEffect : PassiveSkillEffect
+{
+    public List<float> damageAdders = new List<float>() { 0 };
+
+    public override void ApplyPassive(EntityContext context, int stack)
+    {
+        if (stack > damageAdders.Count) return;
+
+        if (context.source is Entity entity)
+        {
+            entity.AddAdder(damageAdders[stack - 1]);
+        }
+    }
+}
