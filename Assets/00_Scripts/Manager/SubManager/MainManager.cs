75e4386f291fac367369f60dd5e80bab74505a3b
iTIC <paul2314@naver.com>
2026-05-04 18:58:39 +0900

    [ui] 로비 화면 UI 수정

--- Assets/00_Scripts/Manager/SubManager/MainManager.cs
+++ Assets/00_Scripts/Manager/SubManager/MainManager.cs
@@ -1,9 +1,67 @@
 using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour
 {
+    public float panelMoveTime = 0.5f;
+    [SerializeField] List<Button> panelButtons = new List<Button>();
+    [SerializeField] RectTransform mainPanel;
+    [SerializeField] Image bgImage;
+
+    [Space]
+    [SerializeField] List<Color> bgColors = new List<Color>();
+
+    private int currentPanelIndex = 0;
+    private Coroutine movePanelCoroutine = null;
+
+    private void Start()
+    {
+        for (int i = 0; i < panelButtons.Count; i++)
+        {
+            int tmp = i;
+            panelButtons[i].onClick.AddListener(() => MovePanel(tmp));
+        }
+
+        bgImage.color = bgColors[0];
+    }
+
+    private void MovePanel(int index)
+    {
+        if (movePanelCoroutine != null) return;
+        if (currentPanelIndex == index) return;
+
+        movePanelCoroutine = StartCoroutine(MovePanelCoroutine(index));
+    }
+
+    private IEnumerator MovePanelCoroutine(int index)
+    {
+        float panelStartPos = mainPanel.anchoredPosition.x;
+        float panelDestPos = 290f - 1040f * index;
+
+        Color bgStartColor = bgColors[currentPanelIndex];
+        Color bgDestColor = bgColors[index];
+
+        float t = 0;
+        while (t < panelMoveTime)
+        {
+            yield return null;
+            t += Time.deltaTime;
+
+            mainPanel.anchoredPosition = new Vector2(Mathf.Lerp(panelStartPos, panelDestPos, MyMath.EaseInOut(t / panelMoveTime)), mainPanel.anchoredPosition.y);
+            bgImage.color = Color.Lerp(bgStartColor, bgDestColor, MyMath.EaseInOut(t / panelMoveTime));
+        }
+
+        mainPanel.anchoredPosition = new Vector3(panelDestPos, mainPanel.anchoredPosition.y);
+        bgImage.color = bgDestColor;
+        currentPanelIndex = index;
+
+        movePanelCoroutine = null;
+    }
+
     public void MoveToScene(string sceneName)
     {
         StartCoroutine(MoveToSceneCoroutine(sceneName));
