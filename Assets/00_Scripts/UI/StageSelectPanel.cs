a5e7cd104c23bfc3f8fab983b3e884edca890d68
iTIC <paul2314@naver.com>
2026-05-06 18:46:18 +0900

    [ui] StageSelect 씬 UI 수정

--- Assets/00_Scripts/UI/StageSelectPanel.cs
+++ Assets/00_Scripts/UI/StageSelectPanel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,12 +8,27 @@ using UnityEngine.UI;
 public class StageSelectPanel : MonoBehaviour
 {
     [SerializeField] GameObject stageSelectPanel;
+    [SerializeField] RectTransform buttonPanel;
+
+    [Space]
+    [SerializeField] TextMeshProUGUI titleText;
+    [SerializeField] string title;
+
+    [Space] 
     [SerializeField] Button learnStageButton;
     [SerializeField] Button[] stageSelectButtons;   // 0: Easy, 1: Hard
     [SerializeField] GameObject[] rewardDecoration;
+    [SerializeField] float panelMoveTime = 0.5f;
+
+    Coroutine panelMoveCoroutine = null;
 
     public void ShowStageSelectPanel()
     {
+        if (panelMoveCoroutine != null) return;
+
+        int day = MANAGER.StudyManager.deck.GetCurrentDay();
+        titleText.text = $"{day + 1}{title}";
+
         if (MANAGER.StudyManager.deck.lastLearnDate.Date != CustomTime.GetTimeNow().Date)   // ¿À´Ã °øºÎ ¾È³¡³ÂÀ¸¸é
         {
             rewardDecoration[0].SetActive(true);
@@ -47,11 +63,71 @@ public class StageSelectPanel : MonoBehaviour
         }
 
         stageSelectPanel.SetActive(true);
+        panelMoveCoroutine = StartCoroutine(ShowPanelCoroutine());
+    }
+
+    private IEnumerator ShowPanelCoroutine()
+    {
+        CanvasGroup group = stageSelectPanel.GetComponent<CanvasGroup>();
+        float startAlpha = 0;
+        float endAlpha = 1;
+
+        Vector2 startPos = new Vector2(buttonPanel.rect.width, 0);
+        Vector2 destPos = Vector2.zero;
+
+        group.alpha = startAlpha;
+        buttonPanel.anchoredPosition = startPos;
+
+        float time = 0;
+        while (time < panelMoveTime)
+        {
+            yield return null;
+            time += Time.deltaTime;
+
+            group.alpha = Mathf.Lerp(startAlpha, endAlpha, MyMath.EaseIn(time / panelMoveTime));
+            buttonPanel.anchoredPosition = Vector2.Lerp(startPos, destPos, MyMath.EaseInOut(time / panelMoveTime));
+        }
+
+        group.alpha = endAlpha;
+        buttonPanel.anchoredPosition = destPos;
+
+        panelMoveCoroutine = null;
     }
 
     public void HideStageSelectPanel()
     {
+        if (panelMoveCoroutine != null) return;
+
+        panelMoveCoroutine = StartCoroutine(HidePanelCoroutine());
+    }
+
+    private IEnumerator HidePanelCoroutine()
+    {
+        CanvasGroup group = stageSelectPanel.GetComponent<CanvasGroup>();
+        float startAlpha = 1;
+        float endAlpha = 0;
+
+        Vector2 startPos = Vector2.zero;
+        Vector2 destPos = new Vector2(buttonPanel.rect.width, 0);
+
+        group.alpha = startAlpha;
+        buttonPanel.anchoredPosition = startPos;
+
+        float time = 0;
+        while (time < panelMoveTime)
+        {
+            yield return null;
+            time += Time.deltaTime;
+
+            group.alpha = Mathf.Lerp(startAlpha, endAlpha, MyMath.EaseOut(time / panelMoveTime));
+            buttonPanel.anchoredPosition = Vector2.Lerp(startPos, destPos, MyMath.EaseInOut(time / panelMoveTime));
+        }
+
+        group.alpha = endAlpha;
+        buttonPanel.anchoredPosition = destPos;
         stageSelectPanel.SetActive(false);
+
+        panelMoveCoroutine = null;
     }
 
     public void SetDifficulty(int diff)
